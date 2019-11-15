df7417483133ee5fee0ad19a7f9b0502ab742517
Ieuan-Horton <55205177+Ieuan-Horton@users.noreply.github.com>
2019-11-15 14:26:04 -0500

    first

---
+++ Project4API/Project4API/Controllers/PaymentGatewayController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Project4API.Controllers
+{
+    [Produces("application/json")]
+    [Route("api/service/PaymentGateway")]
+    public class PaymentGatewayController : Controller
+    {
+        
+    }
+}
